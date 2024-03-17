f249a09658f61d5569353b993d4e6a1f32af60a8
Djboy08 <justindoane@gmail.com>
2024-03-17 19:59:36 -0400

    worker

--- locustfile.py
+++ locustfile.py
@@ -2,8 +2,9 @@ import time
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
-    wait_time = between(1, 5)
+
 
     @task
     def run_get(self):
-        self.client.get("https://items-existing-server-dp8gv.ondigitalocean.app/")
+        # self.client.get("https://items-existing-server-dp8gv.ondigitalocean.app/")
+        self.client.post("https://items-existing-server-dp8gv.ondigitalocean.app/", json={"item_name":"i_am_a_test", "amount": 1})
