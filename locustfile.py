54eb68295fb86dc2d4ac9e8d3a1576d81ff075a3
Djboy08 <justindoane@gmail.com>
2024-03-17 20:16:41 -0400

    constant

--- locustfile.py
+++ locustfile.py
@@ -1,10 +1,10 @@
 import time
-from locust import HttpUser, task, between
+from locust import HttpUser, task, constant
 
 class QuickstartUser(HttpUser):
-
+    wait_time = constant(1)
 
     @task
     def run_get(self):
         # self.client.get("https://items-existing-server-dp8gv.ondigitalocean.app/")
-        self.client.post("https://items-existing-server-dp8gv.ondigitalocean.app/", json={"item_name":"i_am_a_test", "amount": 1})
+        self.client.post("https://items-existing-server-dp8gv.ondigitalocean.app/", json={"item_name":"i_am_a_test2", "amount": 1})
